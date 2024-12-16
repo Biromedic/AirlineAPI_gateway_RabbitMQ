1a6f6483d4185acf8842836dd936f2840f9ae405
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2024-12-16 10:57:26 +0300

    Configure gateway Application for routing

--- gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -13,22 +13,22 @@ public class GatewayConfig {
         return builder.routes()
                 // User Service
                 .route("user_service", r -> r.path("/api/users/**")
-                        .uri("http://localhost:8081"))
+                        .uri("http://localhost:8087"))
                 // Ticket Service
                 .route("ticket_service", r -> r.path("/api/tickets/**")
-                        .uri("http://localhost:8082"))
+                        .uri("http://localhost:8087"))
                 // Listing Service
                 .route("listing_service", r -> r.path("/api/listings/**")
-                        .uri("http://localhost:8083"))
+                        .uri("http://localhost:8087"))
                 // Flight Service
                 .route("flight_service", r -> r.path("/api/flights/**")
-                        .uri("http://localhost:8084"))
+                        .uri("http://localhost:8087"))
                 // Booking Service
                 .route("booking_service", r -> r.path("/api/bookings/**")
-                        .uri("http://localhost:8085"))
+                        .uri("http://localhost:8087"))
                 // Authentication Service
                 .route("auth_service", r -> r.path("/api/auth/**")
-                        .uri("http://localhost:8086"))
+                        .uri("http://localhost:8087"))
                 .route("message_service", r -> r.path("/api/messages/**")
                         .uri("http://localhost:8088"))
                 .route("payment_service", r -> r.path("/api/payments/**")
