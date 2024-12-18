1392e4043329a394c10e6f54774f32c3450f9dc5
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2024-12-18 17:23:02 +0300

    Add Dockerfile and docker-compose

--- gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -13,28 +13,28 @@ public class GatewayConfig {
         return builder.routes()
                 // User Service
                 .route("user_service", r -> r.path("/api/users/**")
-                        .uri("http://localhost:8087"))
+                        .uri("http://airlineapi:8087"))
                 // Ticket Service
                 .route("ticket_service", r -> r.path("/api/tickets/**")
-                        .uri("http://localhost:8087"))
+                        .uri("http://airlineapi:8087"))
                 // Listing Service
                 .route("listing_service", r -> r.path("/api/listings/**")
-                        .uri("http://localhost:8087"))
+                        .uri("http://airlineapi:8087"))
                 // Flight Service
                 .route("flight_service", r -> r.path("/api/flights/**")
-                        .uri("http://localhost:8087"))
+                        .uri("http://airlineapi:8087"))
                 // Booking Service
                 .route("booking_service", r -> r.path("/api/bookings/**")
-                        .uri("http://localhost:8087"))
+                        .uri("http://airlineapi:8087"))
                 // Authentication Service
                 .route("auth_service", r -> r.path("/api/auth/**")
-                        .uri("http://localhost:8087"))
+                        .uri("http://airlineapi:8087"))
                 .route("message_service", r -> r.path("/api/messages/**")
-                        .uri("http://localhost:8088"))
+                        .uri("http://messaging:8088"))
                 .route("payment_service", r -> r.path("/api/payments/**")
-                        .uri("http://localhost:8090"))
+                        .uri("http://paymentservice:8090"))
                 .route("notification_service", r -> r.path("/api/notifications/**")
-                        .uri("http://localhost:8091"))
+                        .uri("http://notificationservice:8091"))
                 .build();
     }
 }
